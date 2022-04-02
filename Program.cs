878299ded6775fa9e19e8c0788d07c26fb6496c6
leeool <lgonsalez02@gmail.com>
2022-04-02 13:35:49 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿double altura, peso;
+
+Console.Write("\nInsira seu peso (kg).....: ");
+peso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Insira a sua altura (m)..: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Seu IMC é de {peso / (Math.Pow(altura,2)):N2}");
